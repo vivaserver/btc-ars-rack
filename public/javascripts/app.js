af3a4f5b64b7348491f762ef7d938ed0e4718284
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2014-06-30 00:26:25 -0300

    concept USD -> ARS exchange by BTC rates

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,17 +3,19 @@ var app;
 app = function() {
   return {
     render: function($id, usd, ars) {
+      var blu = ars/usd;
       numeral.language("en");
       $id.find(".usd").text(numeral(usd).format("0,0.00"));
       numeral.language("es");
       $id.find(".ars").text(numeral(ars).format("0,0.00"));
+      $id.find("span").text(numeral(blu).format("0,0.00"));
     }
   }
 }();
 
 $(document).ready(function() {
   var $buy = $("span#buy"), $sell = $("span#sell");
-  var $updated_at = $("#updated_at")
+  var $updated_at = $("p#updated_at")
 
   $.ajax({
     dataType: "json",
