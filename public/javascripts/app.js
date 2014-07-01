3ba4353509595accee190019f627f207a2212aa4
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2014-07-01 12:39:35 -0300

    concept for caching to localStorage

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -17,20 +17,29 @@ $(document).ready(function() {
   var $buy = $("span#buy"), $sell = $("span#sell");
   var $updated_at = $("p#updated_at")
 
-  $.ajax({
-    dataType: "json",
-    type: "GET",
-    url: "https://digicoins.tk/ajax/get_prices",
-    success: function(data) {
-      console.log(data);
-      if (data.result == "OK") {
-        app.render($buy, data.btcusdask,data.btcarsask);
-        app.render($sell,data.btcusdbid,data.btcarsbid);
-        $updated_at.text(data.quotestime)
+  if (localStorage["digicoins"] == undefined) {
+    $.ajax({
+      dataType: "json",
+      type: "GET",
+      url: "https://digicoins.tk/ajax/get_prices",
+      success: function(data) {
+        if (data.result == "OK") {
+          localStorage["digicoins"] = JSON.stringify(data);
+          app.render($buy, data.btcusdask,data.btcarsask);
+          app.render($sell,data.btcusdbid,data.btcarsbid);
+          $updated_at.text(data.quotestime)
+        }
+      },
+      error: function(xhr, type) {
+        console.log(type);
       }
-    },
-    error: function(xhr, type) {
-      console.log(type);
-    }
-  });
+    });
+  }
+  else {
+    console.log(localStorage["digicoins"]);
+    data = JSON.parse(localStorage["digicoins"]);
+    app.render($buy, data.btcusdask,data.btcarsask);
+    app.render($sell,data.btcusdbid,data.btcarsbid);
+    $updated_at.text(data.quotestime)
+  }
 });
