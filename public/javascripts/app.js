c20400edb86cbef834497e8827de0a5b45d50a2f
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2014-07-01 13:14:12 -0300

    refactor to app Obj.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,45 +1,63 @@
-var app;
+var app = function() {
+  var $buy, $sell, $updated_at;
 
-app = function() {
+  var cached = function() {
+    var cache = localStorage["digicoins"];
+    if (cache != undefined) {
+      return JSON.parse(cache);
+    }
+  };
+
+  var renderUpdateTime = function(dateTime) {
+    $updated_at.text(dateTime)
+  };
+
+  var renderQuote = function($id, usd, ars) {
+    var blu = ars/usd;
+    numeral.language("en");
+    $id.find(".usd").text(numeral(usd).format("0,0.00"));
+    numeral.language("es");
+    $id.find(".ars").text(numeral(ars).format("0,0.00"));
+    $id.find("span").text(numeral(blu).format("0,0.00")+" x USD");
+  };
+
   return {
-    render: function($id, usd, ars) {
-      var blu = ars/usd;
-      numeral.language("en");
-      $id.find(".usd").text(numeral(usd).format("0,0.00"));
-      numeral.language("es");
-      $id.find(".ars").text(numeral(ars).format("0,0.00"));
-      $id.find("span").text(numeral(blu).format("0,0.00")+" x USD");
+    render: function() {
+      if (cached() == undefined) {
+        $.ajax({
+          dataType: "json",
+          type: "GET",
+          url: "https://digicoins.tk/ajax/get_prices",
+          success: function(data) {
+            if (data.result == "OK") {
+              localStorage["digicoins"] = JSON.stringify(data);
+              renderQuote($buy, data.btcusdask,data.btcarsask);
+              renderQuote($sell,data.btcusdbid,data.btcarsbid);
+              renderUpdateTime(data.quotestime)
+            }
+          },
+          error: function(xhr, type) {
+            console.log(type);
+          }
+        });
+      }
+      else {
+        console.log(cached());
+        data = cached();
+        renderQuote($buy, data.btcusdask,data.btcarsask);
+        renderQuote($sell,data.btcusdbid,data.btcarsbid);
+        renderUpdateTime(data.quotestime)
+      }
+    },
+    init: function($el) {
+      $buy  = $el.find("span#buy");
+      $sell = $el.find("span#sell");
+      $updated_at = $("p#updated_at")
     }
   }
 }();
 
 $(document).ready(function() {
-  var $buy = $("span#buy"), $sell = $("span#sell");
-  var $updated_at = $("p#updated_at")
-
-  if (localStorage["digicoins"] == undefined) {
-    $.ajax({
-      dataType: "json",
-      type: "GET",
-      url: "https://digicoins.tk/ajax/get_prices",
-      success: function(data) {
-        if (data.result == "OK") {
-          localStorage["digicoins"] = JSON.stringify(data);
-          app.render($buy, data.btcusdask,data.btcarsask);
-          app.render($sell,data.btcusdbid,data.btcarsbid);
-          $updated_at.text(data.quotestime)
-        }
-      },
-      error: function(xhr, type) {
-        console.log(type);
-      }
-    });
-  }
-  else {
-    console.log(localStorage["digicoins"]);
-    data = JSON.parse(localStorage["digicoins"]);
-    app.render($buy, data.btcusdask,data.btcarsask);
-    app.render($sell,data.btcusdbid,data.btcarsbid);
-    $updated_at.text(data.quotestime)
-  }
+  app.init($(".content"));
+  app.render();
 });
