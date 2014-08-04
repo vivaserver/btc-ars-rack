33648f73b8c1b9e0f5d7b4020050478fd9a3a9c1
Cristian R. Arroyo <cristian.arroyo@vivaserver.com>
2014-08-04 18:24:52 -0300

    just in case, protect ConectaBitcoin qoute against missing data

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -120,25 +120,30 @@ var app = function() {
     var conf = {
       quote: function(data, use_data_time) {
         var data_time = new Date();
-        if (use_data_time) {  // force expired date
-          data_time = new Date(data_time.getFullYear(),data_time.getMonth(),data_time.getDate()-1);
-        }
-        data_time = data_time.toJSON();
+        if ((data || {}).btc_usd && (data || {}).btc_ars) {
+          if (use_data_time) {  // force expired date
+            data_time = new Date(data_time.getFullYear(),data_time.getMonth(),data_time.getDate()-1);
+          }
+          data_time = data_time.toJSON();
 
-        return {
-          buy: {
-            usd:  data.btc_usd.buy,
-            ars:  data.btc_ars.buy,
-            time: data_time
-          },
-          sell: {
-            usd:  data.btc_usd.sell,
-            ars:  data.btc_ars.sell,
-            time: data_time
-          },
-          // blue: usd_ars.sell/usd_ars.buy
-          created_at: data_time  // "2014-07-09T17:13:34.553Z"
-        };
+          return {
+            buy: {
+              usd:  data.btc_usd.buy,
+              ars:  data.btc_ars.buy,
+              time: data_time
+            },
+            sell: {
+              usd:  data.btc_usd.sell,
+              ars:  data.btc_ars.sell,
+              time: data_time
+            },
+            // blue: usd_ars.sell/usd_ars.buy
+            created_at: data_time  // "2014-07-09T17:13:34.553Z"
+          };
+        }
+        else {
+          return false;
+        }
       },
       cache: "/javascripts/cache.conectabitcoin.json",
       name: "conectabitcoin",
